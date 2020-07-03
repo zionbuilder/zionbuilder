967072c86a8dfe9728614aefaa76b36480e1f2ca
raluseru <raluca.serdaru@gmail.com>
2020-07-03 09:13:42 +0300

    Update Masks.php

--- includes/Elements/Masks.php
+++ includes/Elements/Masks.php
@@ -13,47 +13,49 @@ if ( ! defined( 'ABSPATH' ) ) {
  * @package ZionBuilder\Elements
  */
 class Masks {
+	final protected function __construct() {
+	}
 	/**
 	 * Holds a list of masks
 	 *
 	 * @var array<int, string>
 	 */
-	private $shapes = [
-		'shape-oblique'        => [
-			'viewbox' => '0 0 1440 180',
-			'paths'   => [ 'M0 0l1440 180H0V0z' ],
-		],
-		'shape-double'         => [
-			'viewbox' => '0 0 1440 180',
-			'paths'   => [
-				'M0 0l1440 180H0V0z',
-				'M0 50l1440 130H0V50z',
+	public function getshapes() {
+		$shapes = [
+			'shape-oblique'        => [
+				'viewbox' => '0 0 1440 180',
+				'paths'   => [ 'M0 0l1440 180H0V0z' ],
 			],
-		],
-		'shape-oblique-mirror' => [
-			'viewbox' => '0 0 1440 180',
-			'paths'   => [ 'M1440 0v181H0V0l720 179L1440 0z' ],
-		],
-		'shape-curved-mirror'  => [
-			'viewbox' => '0 0 1440 180',
-			'paths'   => [ 'M720 179c260.2 0 505.27-64.75 720-179v180H0V0c214.73 114.25 459.8 179 720 179z' ],
-		],
-		'shape-split'          => [
-			'viewbox' => '0 0 1440 50',
-			'paths'   => [ 'M770 50H670l50-50 50 50z' ],
-		],
-		'shape-wavy'           => [
-			'viewbox' => '0 0 1440 50',
-			'paths'   => [ 'M1260 10.15c-90-10.43-90-10.43-180-2.4s-90 8.03-180 0-90-8.03-180 2.4-90 10.43-180 4.42-90-6.01-180 5.74-90 11.75-180 6.16c-90-5.6-90-5.6-180 5.6V52h1440V10.16c-90 10.42-90 10.42-180-.01z' ],
-		],
-	];
-	final protected function __construct() {
+			'shape-double'         => [
+				'viewbox' => '0 0 1440 180',
+				'paths'   => [
+					'M0 0l1440 180H0V0z',
+					'M0 50l1440 130H0V50z',
+				],
+			],
+			'shape-oblique-mirror' => [
+				'viewbox' => '0 0 1440 180',
+				'paths'   => [ 'M1440 0v181H0V0l720 179L1440 0z' ],
+			],
+			'shape-curved-mirror'  => [
+				'viewbox' => '0 0 1440 180',
+				'paths'   => [ 'M720 179c260.2 0 505.27-64.75 720-179v180H0V0c214.73 114.25 459.8 179 720 179z' ],
+			],
+			'shape-split'          => [
+				'viewbox' => '0 0 1440 50',
+				'paths'   => [ 'M770 50H670l50-50 50 50z' ],
+			],
+			'shape-wavy'           => [
+				'viewbox' => '0 0 1440 50',
+				'paths'   => [ 'M1260 10.15c-90-10.43-90-10.43-180-2.4s-90 8.03-180 0-90-8.03-180 2.4-90 10.43-180 4.42-90-6.01-180 5.74-90 11.75-180 6.16c-90-5.6-90-5.6-180 5.6V52h1440V10.16c-90 10.42-90 10.42-180-.01z' ],
+			],
+		];
+		return $shapes;
 	}
-
 	/*
-	 * Returns array from shape id
+	 * Returns string from shape id
 	 *
-	 * @param array of paths
+	 * @param string $shape The shape id for which the attributes will be retrieved
 	 * @param mixed  $mask
 	 */
 	public function get_mask_paths( $mask = '' ) {
@@ -63,7 +65,7 @@ class Masks {
 			return;
 		}
 
-		$returned_value = self::$shapes[$mask]['paths'];
+		$returned_value = self::getshapes()[$mask]['paths'];
 		return $returned_value;
 	}
 
@@ -80,7 +82,9 @@ class Masks {
 			return;
 		}
 
-		$shape_viewbox = self::$shapes[$mask];
+		$shapes        = self::getshapes();
+		$shape_viewbox = $shapes[$mask];
+
 		return $shape_viewbox['viewbox'];
 	}
 }
