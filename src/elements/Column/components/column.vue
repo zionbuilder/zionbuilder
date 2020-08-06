f45abc87ad74e200a4b92b78d0a06670111aec92
raluseru <raluca.serdaru@gmail.com>
2020-08-06 12:38:38 +0300

    add code

--- src/elements/Column/components/column.vue
+++ src/elements/Column/components/column.vue
@@ -5,18 +5,52 @@
 		:data="data"
 		:tag="htmlTag"
 	>
-		<slot name="start" slot="start" />
-		<slot name="end" slot="start" />
+		<slot
+			name="start"
+			slot="start"
+		/>
+		<SvgMask
+			v-if="topMask!==undefined && topMask.shape"
+			:shapePath="topMask['shape']"
+			:color="topMask['color']"
+			:height="topMask['height']"
+			position="top"
+		/>
+
+		<SvgMask
+			v-if="bottomMask!==undefined && bottomMask.shape"
+			:shapePath="bottomMask['shape']"
+			:color="bottomMask['color']"
+			:height="bottomMask['height']"
+			position="bottom"
+		/>
+		<slot
+			name="end"
+			slot="start"
+		/>
 	</SortableContent>
 </template>
 
 <script>
+import SvgMask from '@/common/components/forms/elements/shape-dividers/SvgMask.vue'
 export default {
 	name: 'zion_column',
+	components: {
+		SvgMask
+	},
 	props: ['options', 'data', 'api'],
 	computed: {
 		htmlTag () {
 			return this.options.tag || 'div'
+		},
+		topMask () {
+			return this.shapes['top']
+		},
+		bottomMask () {
+			return this.shapes['bottom']
+		},
+		shapes () {
+			return this.options.shapes || {}
 		}
 	}
 }
