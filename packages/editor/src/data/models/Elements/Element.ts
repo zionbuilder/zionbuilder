1fbf0e44ecf75480e00cc4becc6c3ee84658fde4
Balasa Sorin Stefan <stefan@zionbuilder.io>
2020-10-07 21:26:13 +0300

    Added elements instance

--- packages/editor/src/data/models/Elements/Element.ts
+++ packages/editor/src/data/models/Elements/Element.ts
@@ -1,23 +1,12 @@
 import Model from '../Model'
-import { getOptionValue } from '@zb/utils'
 
 export default class PageElement extends Model {
 	defaults () {
 		return {
-			element_type: null,
-			options: {},
-			uid: null,
-			content: []
+			id: '',
+			name: '',
+			component: null
 		}
 	}
-
-	get name () {
-		// TODO: implement options system
-		return getOptionValue(this.options, '_advanced_options._element_name') || 'My Name'
-		return this.options.getValue('_advanced_options._element_name')
-	}
-
-	set name (name) {
-		this.options.setValue('_advanced_options._element_name', name)
-	}
 }
+
--- packages/editor/src/index.ts
+++ packages/editor/src/index.ts
@@ -11,6 +11,7 @@ import { install as L18NInstall } from '@zb/i18n'
 import { errorInterceptor } from '@zb/rest'
 import { createInstance } from './utils/events'
 export * as optionsInstance from './manager/options/optionsInstance'
+import { initElements } from './data/elements'
 
 // Components
 import App from './App.vue'
@@ -18,7 +19,7 @@ import App from './App.vue'
 // Data
 import { initPanels } from './data'
 
-
+// It will allow us to add events to both the editor document and iframe document
 const pageEvents = createInstance()
 pageEvents.addDocument(window)
 
@@ -35,12 +36,16 @@ appInstance.use(store)
 // Add error interceptor for API
 errorInterceptor(store)
 
+// Init elements registration
+const elements = new ElementsCollection()
+
 // Add editor methods and utilities to all components
 appInstance.config.globalProperties.$zb = {
 	hooks,
 	appInstance,
 	pageEvents,
-	panels
+	panels,
+	elements
 }
 
 appInstance.mount('#znpb-app')
@@ -49,5 +54,6 @@ appInstance.mount('#znpb-app')
 export {
 	appInstance,
 	pageEvents,
-	panels
+	panels,
+	elements
 }
