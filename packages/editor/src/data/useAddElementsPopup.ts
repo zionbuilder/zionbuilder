e7dc3242e75e7e21b9bd798dc5806009a08bc368
Balasa Sorin Stefan <stefan@zionbuilder.io>
2020-10-20 12:21:04 +0300

    Fix popper not positioning correctly in iframe

--- packages/editor/src/data/useAddElementsPopup.ts
+++ packages/editor/src/data/useAddElementsPopup.ts
@@ -32,27 +32,30 @@ const getOffset = (currentDocument) => {
 
 export function useAddElementsPopup () {
 	const showAddElementsPopup = (element, selector) => {
-		// let modifiers = {}
+		let modifiers = {}
 		// Check if the event comes from iframe
-		// if (selector.value.ownerDocument !== window.document) {
-		// 	// Get the offset
-		// 	const offsets = getOffset(selector.value.ownerDocument)
-		// 	const { left, top } = offsets
-		// 	// modifiers = [
-		// 	// 	{
-		// 	// 		name: 'offset',
-		// 	// 		options: {
-		// 	// 			offset: ({ placement }) => {
-		// 	// 				if (placement === 'bottom') {
-		// 	// 					return [200, 0];
-		// 	// 				} else {
-		// 	// 					return [];
-		// 	// 				}
-		// 	// 			  },
-		// 	// 		},
-		// 	// 	},
-		// 	// ]
-		// }
+		if (selector.value.ownerDocument !== window.document) {
+			// Get the offset
+			const offsets = getOffset(selector.value.ownerDocument)
+			const { left, top } = offsets
+			const positionInIframe = {
+				name: 'fixIframePopper',
+				enabled: true,
+				phase: 'afterRead',
+				fn({ state }) {
+					const { x, y } = state.rects.reference
+					state.rects.reference = {
+						...state.rects.reference,
+						x: x + left,
+						y: y + top
+					}
+				},
+			};
+
+
+			modifiers = [positionInIframe]
+		}
+
 
 		activePopup.value = {
 			element,
