2940034032c8e1c4185ac20150c208dcc2ecef24
raluseru <raluca.serdaru@gmail.com>
2020-10-27 16:55:44 +0200

    add addFilter to admin api

--- packages/admin/src/api.js
+++ packages/admin/src/api.js
@@ -2,14 +2,15 @@ import routes from './router'
 
 // Utils
 import { errorInterceptor } from '@zionbuilder/rest'
-
+import { addFilter } from '@zb/hooks'
 window.zb = window.zb || {}
 
 const api = {
 	routes,
 	interceptors: {
 		errorInterceptor
-	}
+	},
+	addFilter
 }
 
 // Add to global object
