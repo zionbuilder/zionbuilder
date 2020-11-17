7e96b52b84c74c3128f656e6653c8426e59078e6
Balasa Sorin Stefan <stefan@zionbuilder.io>
2020-11-17 12:31:00 +0200

    Fixed font icons not loading
    https://hogash.atlassian.net/browse/ZB-2128

--- packages/webpack-config/src/css.js
+++ packages/webpack-config/src/css.js
@@ -12,28 +12,62 @@ module.exports = (config) => {
 			})
 		],
 		module: {
-			rules: [{
-				test: /\.s[ac]ss$/i,
-				use: [
-					{
-						loader: require('mini-css-extract-plugin').loader,
+			rules: [
+
+				{
+					// find these extensions in our css, copy the files to the outputPath,
+					// and rewrite the url() in our css to point them to the new (copied) location
+					test: /\.(woff(2)?|eot|otf|ttf|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+					use: {
+						loader: 'file-loader',
 						options: {
-							hmr: true
+							name: '[name].[ext]',
+							outputPath: 'fonts',
+							publicPath: '../fonts'
 						}
-					},
-					// Creates `style` nodes from JS strings
-					// 'style-loader',
-					// Translates CSS into CommonJS
-					{
-						loader: require.resolve('css-loader')
-					},
-					// Compiles Sass to CSS
-					{
-						loader: 'sass-loader',
-						options: customLoaderOptions.sass || {}
-					},
-				],
-			}, ],
+					}
+				},
+				{
+					test: /\.css$/i,
+					use: [
+						{
+							loader: require('mini-css-extract-plugin').loader,
+							options: {
+								hmr: true
+							}
+						},
+						{
+							loader: require.resolve('css-loader'),
+							options: {
+								url: false
+							}
+						}
+					],
+				},
+				{
+					test: /\.s[ac]ss$/i,
+					use: [
+						{
+							loader: require('mini-css-extract-plugin').loader,
+							options: {
+								hmr: true
+							}
+						},
+						// Translates CSS into CommonJS
+						{
+							loader: require.resolve('css-loader'),
+							options: {
+								url: true
+							}
+						},
+						// Compiles Sass to CSS
+						{
+							loader: 'sass-loader',
+							options: customLoaderOptions.sass || {}
+						},
+					],
+				},
+			],
 		},
 	}
 }
