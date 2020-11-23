2912c24521286a23148ed341226f400565d555e7
Lalú Selú <raluca.serdaru@gmail.com>
2020-11-23 16:02:52 +0200

    fix global classes not updating name in panelElementOptions

--- packages/editor/src/components/elementOptions/ClassSelectorDropdown.vue
+++ packages/editor/src/components/elementOptions/ClassSelectorDropdown.vue
@@ -151,11 +151,11 @@ export default {
 		filteredClasses () {
 			if (this.keyword.length === 0) {
 				const extraClasses = this.computedValue.map(selector => {
-					let className = this.CSSClasses.find( ({ id }) => id === selector )
+					let className = this.CSSClasses.map(function(classObject) { return classObject.id===selector ? classObject.name : selector})
 					return {
 						type: 'class',
 						selector,
-						name: className.name,
+						name: className[0],
 						deletable: true
 					}
 				})
