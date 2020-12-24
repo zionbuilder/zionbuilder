ee5b4941619fd94dec4c4ba41aa59d6edcc4f59d
raluseru <raluca.serdaru@gmail.com>
2020-12-24 16:29:03 +0200

    Fixed Text element - content in panel element options is not the same with preview

--- packages/elements/src/Text/components/Text.vue
+++ packages/elements/src/Text/components/Text.vue
@@ -7,11 +7,27 @@
 </template>
 
 <script>
-import { computed } from 'vue'
+import { isEqual } from 'lodash-es'
+import { computed, watch } from 'vue'
 export default {
 	name: 'ZionText',
 	props: ['options', 'element', 'api'],
 	setup (props) {
+
+		// watch content in case it is changed from inline editor
+		watch(
+			() => [
+				props.options.content
+			].toString()
+			, (newValue, oldValue) => {
+				if (isEqual(newValue, oldValue)) {
+					return
+				}
+
+				props.element.updateOptionValue('content', newValue)
+
+			})
+
 		const content = computed({
 			get () {
 				return props.options.content
