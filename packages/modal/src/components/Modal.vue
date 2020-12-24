1c3dd3514859b0a8def85ace8df6b0429a74870d
raluseru <raluca.serdaru@gmail.com>
2020-12-24 11:36:34 +0200

    add classes to modals in admin

--- packages/modal/src/components/Modal.vue
+++ packages/modal/src/components/Modal.vue
@@ -210,7 +210,7 @@ export default {
 				modalStyle['position'] = 'absolute'
 			}
 			if (this.fullSize) {
-				modalStyle['max-height'] = '95%'
+				modalStyle['max-height'] = '100%'
 			}
 
 			return modalStyle
