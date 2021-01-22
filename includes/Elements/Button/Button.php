aad133390a6ecf1854c8e442dc7da2eaca85869e
raluseru <raluca.serdaru@gmail.com>
2021-01-22 12:54:12 +0200

    update rendered button attributes

--- includes/Elements/Button/Button.php
+++ includes/Elements/Button/Button.php
@@ -380,16 +380,21 @@ class Button extends Element {
 	 * @return void
 	 */
 	public function render( $options ) {
-		$html_tag         = 'span';
-		$icon_html        = '';
-		$button_text_html = '';
-		$button_text      = $options->get_value( 'button_text', false );
-		$icon             = $options->get_value( 'icon', false );
-		$link             = $options->get_value( 'link', false );
+		$html_tag             = 'span';
+		$icon_html            = '';
+		$button_text_html     = '';
+		$button_text          = $options->get_value( 'button_text', false );
+		$icon                 = $options->get_value( 'icon', false );
+		$link                 = $options->get_value( 'link', false );
+		$button_attr          = $this->render_attributes->get_attributes( 'button_styles' );
+		$combined_button_attr = $this->render_attributes->combine_attributes( $button_attr, [ 'class' => 'zb-el-button' ] );
 
+		$icon_attr          = $this->render_attributes->get_attributes( 'icon_styles' );
+		$combined_icon_attr = $this->render_attributes->combine_attributes( $icon_attr, [ 'class' => 'zb-el-button__icon' ] );
 		if ( ! empty( $link['link'] ) ) {
 			$this->attach_link_attributes( 'button', $link );
 			$html_tag = 'a';
+
 		}
 
 		if ( ! empty( $icon ) ) {
@@ -398,9 +403,7 @@ class Button extends Element {
 				'span',
 				'icon',
 				'',
-				[
-					'class' => 'zb-el-button__icon',
-				]
+				$combined_icon_attr
 			);
 		}
 
@@ -419,9 +422,7 @@ class Button extends Element {
 			$html_tag,
 			'button',
 			[ $icon_html, $button_text_html ],
-			[
-				'class' => 'zb-el-button',
-			]
+			$combined_button_attr
 		);
 	}
 }
