952bf0a29d6f09e81eeee3ad1378ab50f1119bc2
Balasa Sorin Stefan <stefan@zionbuilder.io>
2021-03-15 16:48:32 +0200

    Resolve Icon element cannot change icon background
    Fixes https://github.com/zionbuilder/zionbuilder/issues/385

--- packages/modalJS/src/triggers/pageScroll.js
+++ packages/modalJS/src/triggers/pageScroll.js
@@ -7,19 +7,21 @@ function applyTrigger(instance, options) {
 	}
 	let lastScrollTop = 0
 	let pageHeight = 0
+	let windowHeight = 0
 
 	window.addEventListener('load', onPageLoad)
 	window.addEventListener('scroll', onPageScroll, false)
 
 	function onPageLoad() {
-		pageHeight = window.innerHeight
+		pageHeight = document.body.clientHeight
+		windowHeight = window.innerHeight
 	}
 
 	function onPageScroll(event) {
 		const scrollPercentage = pageHeight * optionsWithDefaults.scrollAmmount / 100
 		const directionDown = window.scrollY > lastScrollTop
 
-		if (optionsWithDefaults.direction === 'down' && directionDown && window.scrollY > scrollPercentage) {
+		if (optionsWithDefaults.direction === 'down' && directionDown && (window.scrollY + windowHeight) >= scrollPercentage) {
 			instance.open()
 			window.removeEventListener('scroll', onPageScroll, false)
 		} else if (optionsWithDefaults.direction === 'up' && !directionDown) {
