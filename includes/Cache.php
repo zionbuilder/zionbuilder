18b42c4ff96a60750b939030caf1961e3a79eddb
Balasa Sorin Stefan <stefan@zionbuilder.io>
2021-05-12 17:49:24 +0300

    Fixed animation delay not working properly (#472)
    Fixed https://hogash.atlassian.net/browse/ZP-382
    Fixed https://hogash.atlassian.net/browse/ZP-381

--- includes/Cache.php
+++ includes/Cache.php
@@ -57,7 +57,6 @@ class Cache {
 		// Enqueue styles
 		if ( ! is_admin() ) {
 			add_action( 'wp_enqueue_scripts', [ $this, 'on_enqueue_scripts' ] );
-			add_action( 'wp_footer', [ $this, 'on_enqueue_scripts' ] );
 		} else {
 			// Register default scripts so we can use them in edit mode
 			add_action( 'zionbuilder/editor/before_scripts', [ $this, 'register_default_scripts' ] );
@@ -99,8 +98,8 @@ class Cache {
 		wp_register_style( 'zion-frontend-animations', plugins_url( 'zionbuilder/assets/vendors/css/animate.css' ), [], Plugin::instance()->get_version() );
 
 		// Animate JS
-		wp_register_script( 'zionbuilder-animatejs', Utils::get_file_url( 'assets/vendors/js/animateJs.js' ), [], Plugin::instance()->get_version(), true );
-		wp_add_inline_script( 'zionbuilder-animatejs', 'animateJs()' );
+		wp_register_script( 'zionbuilder-animatejs', Utils::get_file_url( 'dist/js/animateJs.js' ), [], Plugin::instance()->get_version(), true );
+		wp_add_inline_script( 'zionbuilder-animatejs', 'zb.animateJS()' );
 	}
 
 	/**
