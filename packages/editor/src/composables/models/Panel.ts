68fa83f09055b6a31b50a08ae4112e7d086a7bae
Balasa Sorin Stefan <stefan@zionbuilder.io>
2021-08-26 00:59:51 +0300

    Improvement - Keep only one option panel active (#612)

--- packages/editor/src/composables/models/Panel.ts
+++ packages/editor/src/composables/models/Panel.ts
@@ -17,35 +17,36 @@ export class Panel {
 		unit: 'auto'
 	}
 	panelPos = 1
+	group = null
 
 	constructor(config) {
 		Object.assign(this, config)
 	}
 
-	set (key, value) {
+	set(key, value) {
 		this[key] = value
 	}
 
-	close () {
+	close() {
 		this.isActive = false
 	}
 
-	open () {
+	open() {
 		this.isActive = true
 		const { openPanels } = usePanels()
 
 		// If this panel is part of a group,
 		// close other panels from the same group that are already opened
-		if (this.group !== 'undefined') {
+		if (this.group !== null) {
 			openPanels.value.forEach(panel => {
-				if (typeof panel.group !== 'undefined' && panel.group === this.group && panel !== this) {
+				if (panel.group !== null && panel.group === this.group && panel !== this) {
 					panel.close()
 				}
 			})
 		}
 	}
 
-	toggle () {
-		this.isActive = !this.isActive
+	toggle() {
+		this.isActive ? this.close() : this.open()
 	}
 }
