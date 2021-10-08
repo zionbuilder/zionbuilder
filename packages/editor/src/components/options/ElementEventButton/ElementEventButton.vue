0c0cf7e4b0cc41709e9274c3ad15e66cc31e6efc
Danny <daniel@zionbuilder.io>
2021-10-08 15:45:47 +0300

    Fixes Custom PHP button styling

--- packages/editor/src/components/options/ElementEventButton/ElementEventButton.vue
+++ packages/editor/src/components/options/ElementEventButton/ElementEventButton.vue
@@ -40,7 +40,8 @@ export default {
 <style lang="scss">
 .znpb-option__elementEvent {
 	padding: 15px 37px;
-	color: var(--zb-surface-color);
+	color: var(--zb-secondary-text-color);
+	font-weight: 500;
 	line-height: 1;
 	text-align: center;
 	background-color: var(--zb-secondary-color);
