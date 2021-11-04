f26b4f2cab35849a3b24dd3e20897ca02be521a4
Balasa Sorin Stefan <stefan@zionbuilder.io>
2021-11-04 12:15:35 +0200

    Fixed add elements popup jumps to top left of the page in certain conditions

--- packages/preview/src/components/ElementToolbox/ElementToolbox.vue
+++ packages/preview/src/components/ElementToolbox/ElementToolbox.vue
@@ -75,7 +75,7 @@
 			<div
 				v-if="!isAnyDragging"
 				class="znpb-element-toolbox__add-element-button"
-				@click.stop="toggleAddElementsPopup"
+				@click="toggleAddElementsPopup"
 				ref="addElementsPopupButton"
 			>
 				<Icon
