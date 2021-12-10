39a6faf22e47a8a70012255e0697e44f953eab5c
Balasa Sorin Stefan <stefan@zionbuilder.io>
2021-12-10 11:09:03 +0200

    Updated input margin unit

--- packages/components/src/components/InputMarginPadding/InputMarginPadding.vue
+++ packages/components/src/components/InputMarginPadding/InputMarginPadding.vue
@@ -8,8 +8,9 @@
 					[`znpb-option__marginPadding-${position.position}`]: true
 				}"
 				class="option__value option__valueMargin"
-				@mouseenter="activeHover = position.title"
+				@mouseenter="activeHover = position"
 				@mouseleave="activeHover = null"
+				@click="activePopup = position"
 			>
 				<input
 					type="text"
@@ -20,7 +21,13 @@
 			</div>
 			<div class="option__label">
 				<span class="option__labelTitle">{{$translate('margin')}}</span>
-				<span class="option__link">
+				<span
+					class="option__link"
+					@click="linkValues('margin')"
+					:class="{
+						'option__link--linked': linkedMargin
+					}"
+				>
 					<svg
 						class="zion-svg-inline znpb-editor-icon zion-unlink zion-icon"
 						xmlns="http://www.w3.org/2000/svg"
@@ -47,7 +54,7 @@
 						:cursor="position.svg.cursor"
 						:fill="position.svg.fill"
 						:d="position.svg.d"
-						@mouseenter="activeHover = position.title"
+						@mouseenter="activeHover = position"
 						@mouseleave="activeHover = null"
 					/>
 				</svg>
@@ -61,7 +68,7 @@
 					[`znpb-option__marginPadding-${position.position}`]: true
 				}"
 				class="option__value option__valuePadding"
-				@mouseenter="activeHover = position.title"
+				@mouseenter="activeHover = position"
 				@mouseleave="activeHover = null"
 			>
 				<input
@@ -69,12 +76,19 @@
 					placeholder="-"
 					:value="computedValues[position.position]"
 					readonly
+					@click="activePopup = position"
 				>
 			</div>
 
 			<div class="option__label">
 				<span class="option__labelTitle">{{$translate('padding')}}</span>
-				<span class="option__link">
+				<span
+					class="option__link"
+					:class="{
+						'option__link--linked': linkedPadding
+					}"
+					@click="linkValues('padding')"
+				>
 					<svg
 						class="zion-svg-inline znpb-editor-icon zion-unlink zion-icon"
 						xmlns="http://www.w3.org/2000/svg"
@@ -101,13 +115,36 @@
 						:cursor="position.svg.cursor"
 						:fill="position.svg.fill"
 						:d="position.svg.d"
-						@mouseenter="activeHover = position.title"
+						@mouseenter="activeHover = position"
 						@mouseleave="activeHover = null"
 					/>
 				</svg>
 			</div>
 		</div>
-		<span class="option__info">{{activeHover}}</span>
+		<span
+			class="option__info"
+			v-if="activeHover"
+		>{{activeHover.title}}</span>
+
+		<div
+			class="znpb-option__marginPadding-popupWrapper"
+			v-if="activePopup"
+		>
+			<div>{{activePopup.title}}</div>
+			<Icon
+				icon="close"
+				@click.stop="activePopup = null"
+			/>
+
+			<InputNumberUnit
+				v-model="inputValue"
+				:min="0"
+				:max="999"
+				:units="['px', 'rem', 'pt', 'vh', '%']"
+				:step="1"
+				default-unit="px"
+			/>
+		</div>
 	</div>
 </template>
 
@@ -127,6 +164,7 @@ export default {
 		const marginPositions = [
 			{
 				position: 'margin-top',
+				type: 'margin',
 				title: translate('margin-top'),
 				svg: {
 					cursor: 's-resize',
@@ -136,6 +174,7 @@ export default {
 			},
 			{
 				position: 'margin-right',
+				type: 'margin',
 				title: translate('margin-right'),
 				svg: {
 					cursor: 'w-resize',
@@ -145,6 +184,7 @@ export default {
 			},
 			{
 				position: 'margin-bottom',
+				type: 'margin',
 				title: translate('margin-bottom'),
 				svg: {
 					cursor: 's-resize',
@@ -154,6 +194,7 @@ export default {
 			},
 			{
 				position: 'margin-left',
+				type: 'margin',
 				title: translate('margin-left'),
 				svg: {
 					cursor: 'w-resize',
@@ -162,10 +203,10 @@ export default {
 				}
 			},
 		]
-
 		const paddingPositions = [
 			{
 				position: 'padding-top',
+				type: 'padding',
 				title: translate('padding-top'),
 				svg: {
 					cursor: 's-resize',
@@ -175,6 +216,7 @@ export default {
 			},
 			{
 				position: 'padding-right',
+				type: 'padding',
 				title: translate('padding-right'),
 				svg: {
 					cursor: 'w-resize',
@@ -184,6 +226,7 @@ export default {
 			},
 			{
 				position: 'padding-bottom',
+				type: 'padding',
 				title: translate('padding-bottom'),
 				svg: {
 					cursor: 's-resize',
@@ -193,6 +236,7 @@ export default {
 			},
 			{
 				position: 'padding-left',
+				type: 'padding',
 				title: translate('padding-left'),
 				svg: {
 					cursor: 'w-resize',
@@ -210,6 +254,8 @@ export default {
 		 * Refs
 		 */
 		const activeHover = ref(null)
+		const activePopup = ref(null)
+		const lastChanged = ref(null)
 
 		const computedValues = computed({
 			get () {
@@ -223,15 +269,79 @@ export default {
 
 				return values
 			}, set (newValues) {
-				console.log('New values', newValues);
+				emit('update:modelValue', newValues)
 			}
 		})
 
-		function onValueUpdated (sizePosition, newValue) {
-			computedValues.value = {
-				...this.modelValue,
-				[sizePosition]: newValue
+		const inputValue = computed({
+			get () {
+				return computedValues.value[activePopup.value.position]
+			},
+			set (newValue) {
+				onValueUpdated(activePopup.value.position, activePopup.value.type, newValue)
 			}
+		})
+
+		function onValueUpdated (sizePosition, type, newValue) {
+			const isLinked = type === 'margin' ? linkedMargin : linkedPadding
+
+			// Keep a track of the last changed position so we can use it for linking values
+			lastChanged.value = {
+				position: sizePosition,
+				type: type,
+			}
+
+			if (isLinked.value) {
+				const valuesToUpdate = type === 'margin' ? marginPositions : paddingPositions
+				const updatedValues = {}
+
+				valuesToUpdate.forEach(position => updatedValues[position.position] = newValue)
+
+				computedValues.value = {
+					...props.modelValue,
+					...updatedValues
+				}
+			} else {
+				computedValues.value = {
+					...props.modelValue,
+					[sizePosition]: newValue
+				}
+			}
+		}
+
+		/**
+		 * Values linking
+		 * - If a value was manually changed, change all the values to this value
+		 * - If no value was changed, try to find a modified value and apply it to all
+		 */
+		const linkedMargin = ref(isLinked('margin'))
+		const linkedPadding = ref(isLinked('padding'))
+		function linkValues (type) {
+			const valueToChange = type === 'margin' ? linkedMargin : linkedPadding
+			valueToChange.value = !valueToChange.value
+
+			// Set the same value for all
+			if (valueToChange.value) {
+				if (lastChanged.value && lastChanged.value.type === type) {
+					onValueUpdated(valueToChange.value, type, computedValues.value[lastChanged.value.position])
+				} else {
+					// Find a position that has a saved value
+					const valuesToCheck = type === 'margin' ? marginPositions : paddingPositions
+					const savedValueConfig = valuesToCheck.find(positionConfig => computedValues.value[positionConfig.position] !== 'undefined')
+
+					if (savedValueConfig) {
+						onValueUpdated(savedValueConfig.position, type, computedValues.value[savedValueConfig.position])
+					}
+				}
+			}
+		}
+
+		function isLinked (type) {
+			const valuesToCheck = type === 'margin' ? marginPositions : paddingPositions
+
+			return valuesToCheck.every(position => {
+				return computedValues.value[position.position] && computedValues.value[position.position] === computedValues.value[`${type}-top`]
+			})
 		}
 
 		return {
@@ -241,12 +351,17 @@ export default {
 
 			// Refs
 			activeHover,
+			activePopup,
+			linkedMargin,
+			linkedPadding,
 
 			// Computed
 			computedValues,
+			inputValue,
 
 			// methods
-			onValueUpdated
+			onValueUpdated,
+			linkValues
 		}
 	}
 }
@@ -288,6 +403,17 @@ export default {
 	justify-items: center;
 }
 
+.znpb-option__marginPadding-popupWrapper {
+	position: relative;
+	display: grid;
+	width: 214px;
+	height: 108px;
+	padding: 10px;
+	background: var(--zb-surface-color);
+
+	grid-area: 3 / 3 / span 3 / span 3;
+}
+
 .option__info {
 	display: flex;
 	justify-content: center;
@@ -310,6 +436,7 @@ export default {
 	left: 24px;
 	display: flex;
 	align-items: center;
+	pointer-events: none;
 }
 
 .option__labelTitle {
@@ -326,6 +453,11 @@ export default {
 	color: #858585;
 	font-size: 12px;
 	cursor: pointer;
+	pointer-events: all;
+}
+
+.option__link--linked {
+	color: red;
 }
 
 .option__link svg {
