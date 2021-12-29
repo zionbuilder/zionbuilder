916bb013ff2ce182b6e22bbf980d3ebada6dac33
Danny <daniel@zionbuilder.io>
2021-12-29 22:14:28 +0200

    Added some more element icons
    - Text
    - Post Comments

--- packages/components/src/components/Icon/Icon.vue
+++ packages/components/src/components/Icon/Icon.vue
@@ -249,6 +249,11 @@ svg:not(:root).icon {
 			opacity: 0.5;
 		}
 	}
+	&.zion-element-comments {
+		path:last-child {
+			opacity: 0.5;
+		}
+	}
 	&.zion-element-archive {
 		path:nth-child(5),
 		path:nth-child(6) {
