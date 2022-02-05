96a09f785e137d6a28829bf1b4f3b8a1427af855
Balasa Sorin Stefan <stefan@zionbuilder.io>
2022-02-05 19:18:11 +0200

    Fixed element id not applying

--- zionbuilder.php
+++ zionbuilder.php
@@ -3,7 +3,7 @@
 Plugin Name: Zion Builder
 Plugin URI: https://zionbuilder.io/?utm_campaign=plugin-uri&utm_medium=wp-dashboard-plugins
 Description: The page builder you always wanted. Create any design you want using live editor.
-Version: 3.0.0-beta5
+Version: 3.0.0-beta5.1
 Author: zionbuilder.io
 Author URI: https://zionbuilder.io/?utm_campaign=plugin-uri&utm_medium=wp-dashboard-plugins
 Text Domain: zionbuilder
