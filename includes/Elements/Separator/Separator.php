d3d32229269c0bf287f95df1194c9bccbecac03b
Balasa Sorin Stefan <stefan@zionbuilder.io>
2022-03-18 13:59:05 +0200

    Code cleanup

--- includes/Elements/Separator/Separator.php
+++ includes/Elements/Separator/Separator.php
@@ -340,9 +340,6 @@ class Separator extends Element {
 	/**
 	 * Enqueue element scripts for both frontend and editor
 	 *
-	 * If you want to use the ZionBuilder cache system you must use
-	 * the enqueue_editor_script(), enqueue_element_script() functions
-	 *
 	 * @return void
 	 */
 	public function enqueue_scripts() {
