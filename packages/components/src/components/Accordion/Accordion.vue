581b1fee8ea6df51dde866897951960aef19a45e
Islam Mohamed <65309837+wise-gorilla@users.noreply.github.com>
2022-03-21 15:31:02 +0200

    Refactored Accordion component

--- packages/components/src/components/Accordion/Accordion.vue
+++ packages/components/src/components/Accordion/Accordion.vue
@@ -1,60 +1,44 @@
 <template>
-	<div
-		class="znpb-accordion"
-		:class="{'znpb-accordion--collapsed': localCollapsed}"
-	>
-		<div
-			class="znpb-accordion__header"
-			@click="localCollapsed = !localCollapsed"
-		>
+	<div class="znpb-accordion" :class="{ 'znpb-accordion--collapsed': localCollapsed }">
+		<div class="znpb-accordion__header" @click="localCollapsed = !localCollapsed">
 			<!-- @slot Content that will be placed inside the accordion header -->
-			<slot name="header">{{header}}</slot>
-			<Icon
-				icon="select"
-				class="znpb-accordion-title-icon"
-			/>
+			<slot name="header">{{ header }}</slot>
+			<Icon icon="select" class="znpb-accordion-title-icon" />
 		</div>
-		<div
-			class="znpb-accordion__content"
-			v-if="localCollapsed"
-		>
+		<div v-if="localCollapsed" class="znpb-accordion__content">
 			<!-- @slot Content that will be placed inside the accordion content -->
 			<slot></slot>
 		</div>
 	</div>
 </template>
 
-<script>
-import Icon from '../Icon/Icon.vue'
-
+<script lang="ts">
 export default {
 	name: 'Accordion',
-	components: {
-		Icon
-	},
-	props: {
-		/**
-		 * If the accordion should be open or closed by default
-		 */
-		collapsed: {
-			required: false,
-			type: Boolean,
-			default: false
-		},
-		/**
-		 * Text that will be displayed inside the accordion header
-		 */
-		header: {
-			type: String,
-			required: false
-		}
-	},
-	data () {
-		return {
-			localCollapsed: this.collapsed
-		}
-	}
+};
+</script>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+import Icon from '../Icon/Icon.vue';
+
+interface IProps {
+	/**
+	 * If the accordion should be open or closed by default
+	 */
+	collapsed?: boolean;
+
+	/**
+	 * Text that will be displayed inside the accordion header
+	 */
+	header?: string;
 }
+
+const props = withDefaults(defineProps<IProps>(), {
+	collapsed: false,
+});
+
+const localCollapsed = ref(props.collapsed);
 </script>
 
 <style lang="scss">
