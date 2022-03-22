5b0a7b45c539c81ba140df2e07536bfc9ec75b14
Balasa Sorin Stefan <stefan@zionbuilder.io>
2022-03-22 11:41:04 +0200

    Code cleanup

--- packages/preview/src/composables/useOptions.ts
+++ packages/preview/src/composables/useOptions.ts
@@ -1,47 +1,46 @@
-import { cloneDeep } from 'lodash-es'
-import { applyFilters } from '@zb/hooks'
+import { cloneDeep } from 'lodash-es';
+import { applyFilters } from '@zb/hooks';
 
 export const parseOptions = (options, schema, config) => {
 	// Create a clone
-	options = applyFilters('zionbuilder/options/model', cloneDeep(options), config)
-	const renderAttributes = {}
-	const customCSS = ''
+	options = applyFilters('zionbuilder/options/model', cloneDeep(options), config);
+	const renderAttributes = {};
+	const customCSS = '';
 	const customCSSMap = {
 		default: {},
 		laptop: {},
 		tablet: {},
-		mobile: {}
-	}
+		mobile: {},
+	};
 
 	const startLoading = () => {
 		if (typeof config.onLoadingStart === 'function') {
-			config.onLoadingStart()
+			config.onLoadingStart();
 		}
-	}
+	};
 
 	const endLoading = () => {
 		if (typeof config.onLoadingEnd === 'function') {
-			config.onLoadingEnd()
+			config.onLoadingEnd();
 		}
-	}
+	};
 
 	const parseData = () => {
 		// Allow external data modification
 		const options =
-
-		// Set defaults and extract render attributes and custom css
-		this.parseOptions(this.schema, options)
+			// Set defaults and extract render attributes and custom css
+			this.parseOptions(this.schema, options);
 
 		return {
 			options: options,
 			renderAttributes: this.renderAttributes,
-			customCSS: this.getCustomCSS()
-		}
-	}
+			customCSS: this.getCustomCSS(),
+		};
+	};
 
 	return {
 		renderAttributes,
 		customCSS,
-		options
-	}
-}
+		options,
+	};
+};
