6a3bff2ed401d677201d2e817b1fc76923abe403
Islam Mohamed <65309837+wise-gorilla@users.noreply.github.com>
2022-04-09 12:36:59 +0200

    Refactored InputBorder

--- packages/components/src/components/InputBorders/InputBorderTabs.vue
+++ packages/components/src/components/InputBorders/InputBorderTabs.vue
@@ -1,16 +1,8 @@
 <template>
 	<div class="znpb-input-border-tabs-wrapper">
-		<Tabs
-			tab-style="group"
-			class="znpb-input-border-tabs"
-		>
-			<Tab
-				:name="tab.name"
-				v-for="(tab, index) in positions"
-				:key='index'
-				class="znpb-input-border-tabs__tab"
-			>
-				<template v-slot:title>
+		<Tabs tab-style="group" class="znpb-input-border-tabs">
+			<Tab v-for="tab in positions" :key="tab.id" :name="tab.name" class="znpb-input-border-tabs__tab">
+				<template #title>
 					<div>
 						<Icon :icon="tab.icon"></Icon>
 					</div>
@@ -18,90 +10,86 @@
 
 				<InputBorderControl
 					:modelValue="computedValue[tab.id] || {}"
-					@update:modelValue="onValueUpdated(tab.id, $event)"
+					@update:modelValue="onValueUpdated(tab.id as PositionIds, $event)"
 				></InputBorderControl>
 			</Tab>
 		</Tabs>
 	</div>
 </template>
-<script>
-import { cloneDeep, set, unset } from 'lodash-es'
 
-import { Icon } from '../Icon'
-import InputBorderControl from './InputBorderControl.vue'
-
+<script lang="ts">
 export default {
 	name: 'InputBorderTabs',
-	props: {
-		/**
-		 * v-model/modelValue for borders
-		 */
-		modelValue: {
-			default () {
-				return {}
-			},
-			type: Object,
-			required: false
-		}
-	},
-	components: {
-		InputBorderControl,
-		Icon
-	},
-	data () {
-		return {
-			positions: {
-				all: {
-					name: 'all',
-					icon: 'all-sides',
-					id: 'all'
-				},
-				top: {
-					name: 'Top',
-					icon: 'border-top',
-					id: 'top'
-				},
-				right: {
-					name: 'right',
-					icon: 'border-right',
-					id: 'right'
-				},
-				bottom: {
-					name: 'bottom',
-					icon: 'border-bottom',
-					id: 'bottom'
-				},
-				left: {
-					name: 'left',
-					icon: 'border-left',
-					id: 'left'
-				}
-			}
-		}
-	},
-	computed: {
-		computedValue () {
-			return this.modelValue || {}
-		}
-	},
-	methods: {
-		onValueUpdated (position, newValue) {
-			const clonedValue = cloneDeep(this.modelValue)
-			if (null === newValue) {
-				unset(clonedValue, position)
-			} else {
-				set(clonedValue, position, newValue)
-			}
+};
+</script>
 
-			if (Object.keys(clonedValue).length > 0) {
-				this.$emit('update:modelValue', clonedValue)
-			} else {
-				this.$emit('update:modelValue', null)
-			}
-		}
+<script lang="ts" setup>
+import { computed } from 'vue';
+import { cloneDeep, set, unset } from 'lodash-es';
+import { Icon } from '../Icon';
+import InputBorderControl, { BorderValue } from './InputBorderControl.vue';
+
+type PositionIds = 'all' | 'top' | 'right' | 'bottom' | 'left';
+type ModelValue = Partial<Record<PositionIds, BorderValue>>;
+
+const props = defineProps<{
+	modelValue?: ModelValue;
+}>();
+
+const emit = defineEmits<{
+	(e: 'update:modelValue', value: ModelValue | null): void;
+}>();
+
+const positions: {
+	name: string;
+	icon: string;
+	id: PositionIds;
+}[] = [
+	{
+		name: 'all',
+		icon: 'all-sides',
+		id: 'all',
+	},
+	{
+		name: 'Top',
+		icon: 'border-top',
+		id: 'top',
+	},
+	{
+		name: 'right',
+		icon: 'border-right',
+		id: 'right',
+	},
+	{
+		name: 'bottom',
+		icon: 'border-bottom',
+		id: 'bottom',
+	},
+	{
+		name: 'left',
+		icon: 'border-left',
+		id: 'left',
+	},
+];
+
+const computedValue = computed(() => props.modelValue || {});
+
+function onValueUpdated(position: PositionIds, newValue: BorderValue) {
+	const clonedValue: ModelValue = cloneDeep(props.modelValue || {});
+	if (newValue === null) {
+		unset(clonedValue, position);
+	} else {
+		set(clonedValue, position, newValue);
+	}
+
+	if (Object.keys(clonedValue).length > 0) {
+		emit('update:modelValue', clonedValue);
+	} else {
+		emit('update:modelValue', null);
 	}
 }
 </script>
+
 <style lang="scss">
 .znpb-input-border-tabs-wrapper {
 	.znpb-options-form-wrapper {
