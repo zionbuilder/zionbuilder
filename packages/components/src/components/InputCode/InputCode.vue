b269ed6c08a710cf7157da35277b8a3ece89b57d
Islam Mohamed <65309837+wise-gorilla@users.noreply.github.com>
2022-04-17 22:39:04 +0200

    Refactored InputCode

--- packages/components/src/components/InputCode/InputCode.vue
+++ packages/components/src/components/InputCode/InputCode.vue
@@ -1,140 +1,142 @@
 <template>
 	<div class="znpb-custom-code">
 		<textarea
-			class="znpb-custom-code__text-area"
 			ref="codeMirrorTextarea"
-			v-model="modelValue"
+			:value="modelValue"
+			class="znpb-custom-code__text-area"
 			:placeholder="placeholder"
 		></textarea>
 	</div>
 </template>
 
-<script>
-import { ref, watch, onMounted } from 'vue'
-
+<script lang="ts">
 export default {
 	name: 'InputCode',
-	props: {
-		placeholder: {
-			type: String
-		},
-		mode: {
-			type: String
-		},
-		modelValue: {
-			type: String,
-			required: false,
-			default () {
-				return ''
-			}
+};
+</script>
+
+<script lang="ts" setup>
+import { ref, Ref, watch, onMounted } from 'vue';
+import type { Editor } from 'codemirror';
+
+const props = withDefaults(
+	defineProps<{
+		placeholder?: string;
+		mode?: string;
+		modelValue?: string;
+	}>(),
+	{
+		modelValue: '',
+	},
+);
+
+const emit = defineEmits<{
+	(e: 'update:modelValue', value: string): void;
+}>();
+
+let editor: Editor;
+const codeMirrorTextarea: Ref<HTMLTextAreaElement | null> = ref(null);
+let ignoreChange = false;
+
+watch(
+	() => props.modelValue,
+	newValue => {
+		if (editor?.getValue() !== newValue) {
+			ignoreChange = true;
+			editor.setValue(newValue);
 		}
 	},
-	setup (props, { emit }) {
-		let editor = null
-		const codeMirrorTextarea = ref(null)
-		let ignoreChange = false
-		watch(() => props.modelValue, (newValue) => {
-			if (editor && editor.getValue() !== newValue) {
-				ignoreChange = true
-				editor.setValue(newValue)
-			}
-		})
+);
 
-		function onEditorChange (instance) {
-			if (ignoreChange === false) {
-				emit('update:modelValue', instance.getValue())
-			}
-			ignoreChange = false
-		}
-
-		const lint = [
-			'text/css',
-			'text/x-scss',
-			'text/x-less',
-			'text/javascript',
-			'application/json',
-			'application/ld+json',
-			'text/typescript',
-			'application/typescript',
-			'htmlmixed',
-		].includes(props.mode) ? true : false
-
-		onMounted(() => {
-			editor = window.wp.CodeMirror.fromTextArea(codeMirrorTextarea.value, {
-				mode: props.mode,
-				lineNumbers: true,
-				lineWrapping: true,
-				lint,
-				autoCloseBrackets: true,
-				matchBrackets: true,
-				autoCloseTags: true,
-				matchTags: {
-					bothTags: true
-				},
-				csslint: {
-					errors: true,
-					'box-model': true,
-					'display-property-grouping': true,
-					'duplicate-properties': true,
-					'known-properties': true,
-					'outline-none': true
-				},
-				jshint: {
-					'boss': true,
-					'curly': true,
-					'eqeqeq': true,
-					'eqnull': true,
-					'es3': true,
-					'expr': true,
-					'immed': true,
-					'noarg': true,
-					'nonbsp': true,
-					'onevar': true,
-					'quotmark': 'single',
-					'trailing': true,
-					'undef': true,
-					'unused': true,
-					'browser': true,
-					'globals': {
-						'_': false,
-						'Backbone': false,
-						'jQuery': false,
-						'JSON': false,
-						'wp': false
-					}
-				},
-				htmlhint: {
-					'tagname-lowercase': true,
-					'attr-lowercase': true,
-					'attr-value-double-quotes': false,
-					'doctype-first': false,
-					'tag-pair': true,
-					'spec-char-escape': true,
-					'id-unique': true,
-					'src-not-empty': true,
-					'attr-no-duplication': true,
-					'alt-require': true,
-					'space-tab-mixed-disabled': 'tab',
-					'attr-unsafe-chars': true
-				},
-				// Show lint error numbers
-				gutters: ['CodeMirror-lint-markers']
-			})
-
-			// Set the editor value
-			if (props.modelValue) {
-				// editor.setValue(this.modelValue)
-			}
-
-			// Bind events
-			editor.on('change', onEditorChange)
-		})
-
-		return {
-			codeMirrorTextarea
-		}
+function onEditorChange(instance: Editor) {
+	if (!ignoreChange) {
+		emit('update:modelValue', instance.getValue());
 	}
+	ignoreChange = false;
 }
+
+const lint = [
+	'text/css',
+	'text/x-scss',
+	'text/x-less',
+	'text/javascript',
+	'application/json',
+	'application/ld+json',
+	'text/typescript',
+	'application/typescript',
+	'htmlmixed',
+].includes(props.mode || '');
+
+onMounted(() => {
+	editor = window.wp.CodeMirror.fromTextArea(codeMirrorTextarea.value, {
+		mode: props.mode,
+		lineNumbers: true,
+		lineWrapping: true,
+		lint,
+		autoCloseBrackets: true,
+		matchBrackets: true,
+		autoCloseTags: true,
+		matchTags: {
+			bothTags: true,
+		},
+		csslint: {
+			errors: true,
+			'box-model': true,
+			'display-property-grouping': true,
+			'duplicate-properties': true,
+			'known-properties': true,
+			'outline-none': true,
+		},
+		jshint: {
+			boss: true,
+			curly: true,
+			eqeqeq: true,
+			eqnull: true,
+			es3: true,
+			expr: true,
+			immed: true,
+			noarg: true,
+			nonbsp: true,
+			onevar: true,
+			quotmark: 'single',
+			trailing: true,
+			undef: true,
+			unused: true,
+			browser: true,
+			globals: {
+				_: false,
+				Backbone: false,
+				jQuery: false,
+				JSON: false,
+				wp: false,
+			},
+		},
+		htmlhint: {
+			'tagname-lowercase': true,
+			'attr-lowercase': true,
+			'attr-value-double-quotes': false,
+			'doctype-first': false,
+			'tag-pair': true,
+			'spec-char-escape': true,
+			'id-unique': true,
+			'src-not-empty': true,
+			'attr-no-duplication': true,
+			'alt-require': true,
+			'space-tab-mixed-disabled': 'tab',
+			'attr-unsafe-chars': true,
+		},
+		// Show lint error numbers
+		gutters: ['CodeMirror-lint-markers'],
+	});
+
+	// Set the editor value
+	if (props.modelValue) {
+		// editor.setValue(this.modelValue)
+	}
+
+	// Bind events
+	editor.on('change', onEditorChange);
+});
 </script>
 
 <style lang="scss">
@@ -185,7 +187,14 @@ export default {
 		border-color: #2c89df;
 	}
 
-	.cm-atom, .cm-keyword, .cm-builtin, .cm-meta, .cm-qualifier, .cm-type, .cm-variable-3, .cm-s-default .cm-string {
+	.cm-atom,
+	.cm-keyword,
+	.cm-builtin,
+	.cm-meta,
+	.cm-qualifier,
+	.cm-type,
+	.cm-variable-3,
+	.cm-s-default .cm-string {
 		color: #2c89df;
 	}
 
@@ -198,7 +207,8 @@ export default {
 		color: #525252;
 	}
 
-	.cm-invalidchar, .cm-s-default .cm-error {
+	.cm-invalidchar,
+	.cm-s-default .cm-error {
 		color: #c15050;
 	}
 
