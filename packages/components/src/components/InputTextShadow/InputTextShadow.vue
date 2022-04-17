68197a95c07265a1288327e7358d01bcc71b4a63
Islam Mohamed <65309837+wise-gorilla@users.noreply.github.com>
2022-04-17 02:44:01 +0200

    Refactored InputTextShadow

--- packages/components/src/components/InputTextShadow/InputTextShadow.vue
+++ packages/components/src/components/InputTextShadow/InputTextShadow.vue
@@ -1,76 +1,63 @@
 <template>
-	<div
-		class="znpb-shadow-option-wrapper__outer"
-		:class="`znpb-shadow-option--${shadow_type}`"
-	>
-		<OptionsForm
-			:schema="schema"
-			v-model="valueModel"
-			class="znpb-shadow-option"
-		/>
+	<div class="znpb-shadow-option-wrapper__outer" :class="`znpb-shadow-option--${shadow_type}`">
+		<OptionsForm v-model="valueModel" :schema="schema" class="znpb-shadow-option" />
 	</div>
 </template>
-<script>
-import { computed } from 'vue'
-import { useOptionsSchemas } from '@composables/useOptionsSchemas'
 
+<script lang="ts">
 export default {
 	name: 'InputTextShadow',
-	props: {
-		/**
-		* Value of text shadow
-		*/
-		modelValue: {
-			type: Object,
-			required: false,
-			default () {
-				return {}
-			}
-		},
-		/**
-		* Boolean if the shadow is inset or not
-		*/
-		inset: {
-			type: Boolean,
-			required: false,
-			default: false
-		},
-		/**
-		* String represanting the shadow type
-		*/
-		shadow_type: {
-			type: String,
-			required: false,
-			default: 'text-shadow'
-		}
-	},
-	setup (props, { emit }) {
-		const { getSchema } = useOptionsSchemas()
-		const schema = computed(() => {
-			if (props.shadow_type === 'text-shadow') {
-				const { inset, spread, ...shadowSchema } = getSchema('shadowSchema')
-				return shadowSchema
-			}
-
-			return getSchema('shadowSchema')
-		})
-
-		const valueModel = computed({
-			get () {
-				return props.modelValue || {}
-			},
-			set (newValue) {
-				emit('update:modelValue', newValue)
-			}
-		})
-		return {
-			getSchema,
-			schema,
-			valueModel
-		}
-	}
-}
+};
 </script>
+
+<script lang="ts" setup>
+import { computed } from 'vue';
+import { useOptionsSchemas } from '@composables/useOptionsSchemas';
+
+interface Shadow {
+	'offset-x'?: string;
+	'offset-y'?: string;
+	blur?: string;
+	color?: string;
+}
+
+const props = withDefaults(
+	defineProps<{
+		modelValue?: Shadow;
+		inset?: boolean;
+		shadow_type?: string;
+	}>(),
+	{
+		modelValue: () => {
+			return {};
+		},
+		shadow_type: 'text-shadow',
+	},
+);
+
+const emit = defineEmits(['update:modelValue']);
+
+const { getSchema } = useOptionsSchemas();
+
+const schema = computed(() => {
+	if (props.shadow_type === 'text-shadow') {
+		const { inset, spread, ...shadowSchema } = getSchema('shadowSchema');
+		return shadowSchema;
+	}
+
+	return getSchema('shadowSchema');
+});
+
+const valueModel = computed({
+	get() {
+		return props.modelValue || {};
+	},
+	set(newValue: Shadow) {
+		emit('update:modelValue', newValue);
+	},
+});
+</script>
+
 <style lang="scss">
 .znpb-shadow-option {
 	display: flex;
@@ -90,7 +77,7 @@ export default {
 		.znpb-shadow-option__colorpicker.znpb-input-wrapper {
 			width: 100% !important;
 
-			 > * {
+			> * {
 				width: calc(50% - 5px);
 			}
 		}
