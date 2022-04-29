42ddb0b2c86bc7f92d57ca143d58a63de6cc4eff
Balasa Sorin Stefan <stefan@zionbuilder.io>
2022-04-29 09:42:09 +0300

    Fixed Image slider doesn't properly work inside tabs

--- packages/elements/src/ImageSlider/frontend.js
+++ packages/elements/src/ImageSlider/frontend.js
@@ -1,85 +1,87 @@
-window.zbFrontend = window.zbFrontend || []
-window.zbFrontend.scripts = window.zbFrontend.scripts || {}
+window.zbFrontend = window.zbFrontend || [];
+window.zbFrontend.scripts = window.zbFrontend.scripts || {};
 
-function useSwiper () {
+function useSwiper() {
 	function getConfig(sliderEl) {
-		const configAttr = sliderEl.dataset.zionSliderConfig
-		const elementConfig = configAttr ? JSON.parse(configAttr) : {}
+		const configAttr = sliderEl.dataset.zionSliderConfig;
+		const elementConfig = configAttr ? JSON.parse(configAttr) : {};
 		const sliderConfig = {
 			autoplay: true,
-			autoHeight: true
-		}
+			autoHeight: true,
+		};
 
 		// Pagination
 		if (elementConfig.pagination) {
 			sliderConfig.pagination = {
-				el: '.swiper-pagination'
-			}
+				el: '.swiper-pagination',
+			};
 		}
 
 		// Navigation
 		if (elementConfig.arrows) {
 			sliderConfig.navigation = {
 				nextEl: '.swiper-button-next',
-				prevEl: '.swiper-button-prev'
-			}
+				prevEl: '.swiper-button-prev',
+			};
 		}
 
 		// Slides to show and breakpoints
-		let slidesPerView = 1
-		let slidesToShow = elementConfig.slides_to_show || 1
-		const breakpoints = {}
+		let slidesPerView = 1;
+		let slidesToShow = elementConfig.slides_to_show || 1;
+		const breakpoints = {};
 
 		if (typeof slidesToShow === 'number') {
-			slidesPerView = slidesToShow
+			slidesPerView = slidesToShow;
 		} else if (typeof slidesToShow === 'object') {
-			slidesPerView = typeof slidesToShow.default !== 'undefined' ? slidesToShow.default : 1
+			slidesPerView = typeof slidesToShow.default !== 'undefined' ? slidesToShow.default : 1;
 
-			let lastValue = false
+			let lastValue = false;
 			Object.keys(window.zbFrontendResponsiveDevicesMobileFirst).forEach(key => {
-				const value = window.zbFrontendResponsiveDevicesMobileFirst[key]
+				const value = window.zbFrontendResponsiveDevicesMobileFirst[key];
 				if (typeof slidesToShow[key] !== 'undefined') {
 					breakpoints[value] = {
-						slidesPerView: slidesToShow[key]
-					}
-					lastValue = slidesToShow[key]
+						slidesPerView: slidesToShow[key],
+					};
+					lastValue = slidesToShow[key];
 				} else if (lastValue !== false) {
 					breakpoints[value] = {
-						slidesPerView: lastValue
-					}
+						slidesPerView: lastValue,
+					};
 				}
-			})
+			});
 		}
 
-		sliderConfig.slidesPerView = slidesPerView
-		sliderConfig.breakpoints = breakpoints
+		sliderConfig.slidesPerView = slidesPerView;
+		sliderConfig.breakpoints = breakpoints;
 
 		return {
 			...sliderConfig,
-			...elementConfig.rawConfig
-		}
+			...elementConfig.rawConfig,
+			observer: true,
+			observeParents: true,
+		};
 	}
 
 	function initSlider(sliderEl, config) {
-		return new window.Swiper(sliderEl, config)
+		return new window.Swiper(sliderEl, config);
 	}
 
 	function runAll(scope = document) {
-		const sliders = scope.querySelectorAll('.zb-el-imageSlider')
+		const sliders = scope.querySelectorAll('.zb-el-imageSlider');
 		sliders.forEach(sliderEl => {
-			const config = getConfig(sliderEl)
-			sliderEl.zbSwiper = initSlider(sliderEl, config)
-		})
+			const config = getConfig(sliderEl);
+			sliderEl.zbSwiper = initSlider(sliderEl, config);
+		});
 	}
 
 	return {
 		getConfig,
 		initSlider,
-		runAll
-	}
+		runAll,
+	};
 }
 
 // Register script
-window.zbFrontend.scripts.swiper = useSwiper()
+window.zbFrontend.scripts.swiper = useSwiper();
 // Run on current page
-window.zbFrontend.scripts.swiper.runAll()
+window.zbFrontend.scripts.swiper.runAll();
