ab5437ad06abb8967aac539d0703d64168c15d14
Balasa Sorin Stefan <stefan@zionbuilder.io>
2022-05-12 13:52:35 +0300

    Code cleanup

--- packages/editor/src/components/options/AccordionMenu/AccordionMenu.vue
+++ packages/editor/src/components/options/AccordionMenu/AccordionMenu.vue
@@ -1,49 +1,29 @@
 <template>
 	<HorizontalAccordion
+		ref="accordion"
 		class="znpb-option-layout__menu"
 		:title="title"
 		:icon="$attrs.icon"
 		:show-back-button="true"
 		:show-home-button="true"
 		:home-button-text="homeButtonText || 'Options'"
+		:has-breadcrumbs="showBreadcrumbs"
 		@expand="onAccordionExpanded"
 		@collapse="onAccordionCollapsed"
-		:has-breadcrumbs="showBreadcrumbs"
-		ref="accordion"
 	>
-
-		<template
-			v-if="hasHeaderSlot"
-			#header
-		>
+		<template v-if="hasHeaderSlot" #header>
 			<slot name="header"></slot>
 		</template>
 
-		<template
-			v-if="hasTitleSlot"
-			#title
-		>
+		<template v-if="hasTitleSlot" #title>
 			<slot name="title"></slot>
 		</template>
 
-		<template
-			v-else
-			#title
-		>
-			<Icon
-				v-if="$attrs.icon"
-				:icon="$attrs.icon"
-			/>
+		<template v-else #title>
+			<Icon v-if="$attrs.icon" :icon="$attrs.icon" />
 			<span v-html="title"></span>
-			<ZionLabel
-				v-if="label"
-				:text="label.text"
-				:type="label.type"
-			/>
-			<ChangesBullet
-				v-if="showChanges && hasChanges"
-				@remove-styles="$emit('update:modelValue', null)"
-			/>
+			<ZionLabel v-if="label" :text="label.text" :type="label.type" />
+			<ChangesBullet v-if="showChanges && hasChanges" @remove-styles="$emit('update:modelValue', null)" />
 		</template>
 
 		<template #actions>
@@ -51,119 +31,121 @@
 		</template>
 
 		<OptionsForm
+			v-if="expanded"
+			v-model="optionsValue"
 			class="znpb-option-layout__menu-options-form"
 			:schema="child_options"
-			v-model="optionsValue"
 			:show-changes="showChanges"
-			v-if="expanded"
 		/>
 	</HorizontalAccordion>
 </template>
 
 <script>
-import { computed } from 'vue'
+import { computed, inject } from 'vue';
 
-import ZionLabel from '../../../common/Label.vue'
+import ZionLabel from '../../../common/Label.vue';
 
 export default {
 	name: 'LayoutMenu',
+	components: {
+		ZionLabel,
+	},
 	inject: {
 		parentAccordion: {
-			default: null
+			default: null,
 		},
 		showChanges: {
-			default: true
-		}
-	},
-	components: {
-		ZionLabel
+			default: true,
+		},
 	},
 	props: {
 		child_options: {
 			type: Object,
-			required: false
+			required: false,
 		},
 		title: {
 			type: String,
-			required: true
+			required: true,
 		},
 		modelValue: {
-			required: false
+			required: false,
 		},
 		homeButtonText: {
-			type: String
+			type: String,
 		},
 		add_to_parent_breadcrumbs: {
 			type: Boolean,
 			required: false,
-			default: false
+			default: false,
 		},
 		label: {
 			type: Object,
-			required: false
-		}
+			required: false,
+		},
 	},
-	setup (props, { slots }) {
-		const hasHeaderSlot = computed(() => !!slots.header)
-		const hasTitleSlot = computed(() => !!slots.title)
+	setup(props, { slots }) {
+		const hasHeaderSlot = computed(() => !!slots.header);
+		const hasTitleSlot = computed(() => !!slots.title);
+		const InputWrapper = inject('inputWrapper');
+		const hasChanges = computed(() => {
+			return InputWrapper.hasChanges;
+		});
 
 		return {
 			hasHeaderSlot,
-			hasTitleSlot
-		}
+			hasTitleSlot,
+			hasChanges,
+		};
 	},
-	computed: {
-		valueModel () {
-			return this.modelValue || {}
-		},
-		hasChanges () {
-			return this.$parent.hasChanges
-		},
-		optionsValue: {
-			get () {
-				return this.modelValue || {}
-			},
-			set (newValue) {
-				this.$emit('update:modelValue', newValue)
-			}
-		}
-	},
-	data () {
+	data() {
 		return {
 			showBreadcrumbs: this.parentAccordion === null,
-			expanded: false
+			expanded: false,
+		};
+	},
+	computed: {
+		valueModel() {
+			return this.modelValue || {};
+		},
+		optionsValue: {
+			get() {
+				return this.modelValue || {};
+			},
+			set(newValue) {
+				this.$emit('update:modelValue', newValue);
+			},
+		},
+	},
+	mounted() {
+		this.$el.parentNode.classList.add('znpb-option-layout__menu-container');
+	},
+	beforeUnmount() {
+		if (this.breadCrumbConfig) {
+			this.parentAccordion.removeBreadcrumb(this.breadCrumbConfig);
 		}
 	},
 	methods: {
-		onAccordionExpanded () {
+		onAccordionExpanded() {
 			if (this.parentAccordion !== null) {
 				this.breadCrumbConfig = {
 					title: this.title,
-					previousCallback: this.$refs.accordion.closeAccordion
-				}
+					previousCallback: this.$refs.accordion.closeAccordion,
+				};
 
-				this.parentAccordion.addBreadcrumb(this.breadCrumbConfig)
+				this.parentAccordion.addBreadcrumb(this.breadCrumbConfig);
 			}
 
-			this.expanded = true
+			this.expanded = true;
 		},
-		onAccordionCollapsed () {
+		onAccordionCollapsed() {
 			if (this.parentAccordion !== null && this.parentAccordion) {
-				this.parentAccordion.removeBreadcrumb(this.breadCrumbConfig)
+				this.parentAccordion.removeBreadcrumb(this.breadCrumbConfig);
 			}
 
-			this.expanded = false
-		}
+			this.expanded = false;
+		},
 	},
-	mounted () {
-		this.$el.parentNode.classList.add('znpb-option-layout__menu-container')
-	},
-	beforeUnmount () {
-		if (this.breadCrumbConfig) {
-			this.parentAccordion.removeBreadcrumb(this.breadCrumbConfig)
-		}
-	}
-}
+};
 </script>
 <style lang="scss">
 .znpb-options-form-wrapper .znpb-input-type--accordion_menu {
@@ -177,10 +159,7 @@ export default {
 .znpb-option-layout__menu {
 	padding: 0 5px;
 
-	&
-		> .znpb-horizontal-accordion__content
-		> .znpb-horizontal-accordion-wrapper
-		> &-options-form {
+	& > .znpb-horizontal-accordion__content > .znpb-horizontal-accordion-wrapper > &-options-form {
 		overflow: visible;
 		padding-top: 0;
 	}
