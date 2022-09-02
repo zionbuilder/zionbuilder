88f8245a3507b3bdb56eac37667932b30b2d23de
Balasa Sorin Stefan <stefan@zionbuilder.io>
2022-09-02 11:38:45 +0300

    Added scripts for build and zip

--- bin/build.mjs
+++ bin/build.mjs
@@ -4,10 +4,8 @@ import { filesMap } from './map.mjs';
 import path from 'path';
 import { minify } from 'rollup-plugin-esbuild';
 import { generateManifest } from './manifest.mjs';
-
 import fs from 'fs';
 
-// TODO: clean dist folder before start
 // Clear the dist folder
 const dist = path.resolve('./dist');
 fs.rmSync(dist, { recursive: true, force: true });
@@ -26,13 +24,6 @@ filesMap.forEach(async script => {
       cssCodeSplit: false,
       emptyOutDir: false,
       target: 'es2015',
-      //   lib: {
-      // 	entry: script.input,
-      // 	name: script.name,
-      // 	fileName: (format) => `${script.output}.js`,
-      // 	formats: ['iife']
-      //   },
-
       rollupOptions: {
         input: {
           [script.output]: script.input,
@@ -53,13 +44,6 @@ filesMap.forEach(async script => {
         },
       },
     },
-    // css: {
-    //   preprocessorOptions: {
-    //     scss: {
-    //       additionalData: `@import "../src/common/scss/_mixins.scss";`,
-    //     },
-    //   },
-    // },
     plugins: [vue()],
   });
 });
