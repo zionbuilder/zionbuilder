f7cf30ad47ba9b6b77b8db6d07c62f7d68c7b9f2
Balasa Sorin Stefan <stefan@zionbuilder.io>
2022-10-13 18:09:58 +0300

    Added friendly admin message for errors generated by the PHP code placed inside the code element

--- includes/Elements/Element.php
+++ includes/Elements/Element.php
@@ -1472,7 +1472,7 @@ class Element {
 
 	public function render_placeholder_info( $config ) {
 		// Only render the placeholder in server render
-		if ( ! $this->is_server_render ) {
+		if ( ! current_user_can( 'administrator' ) ) {
 			return;
 		}
 
@@ -1494,6 +1494,76 @@ class Element {
 				<?php echo $config['description']; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
 				</p>
 			</div>
+		<?php
+	}
+
+	public function render_admin_info_text( $config ) {
+		// Only render the placeholder in server render
+		if ( ! current_user_can( 'administrator' ) ) {
+			return;
+		}
+
+		$config = wp_parse_args(
+			$config,
+			array(
+				'title'       => esc_html__( 'Heads up!', 'zionbuilder' ),
+				'description' => '',
+				'type'        => 'info',
+			)
+		);
+
+		?>
+			<div class="znpb-el-notice znpb-el-notice--<?php echo $config['type']; ?>">
+				<span class="znpb-editor-icon-wrapper">
+					<svg class="zion-svg-inline znpb-editor-icon zion-icon zion-desktop" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" viewBox="0 0 50 50" preserveAspectRatio=""><path d="M25.0001 0C31.6306 0 37.9892 2.63403 42.6777 7.32234C47.3664 12.0106 50 18.37 50 24.9999V50H25C18.3698 50 12.0108 47.366 7.32234 42.6777C2.63404 37.9894 1.50167e-06 31.63 1.50167e-06 25C1.50167e-06 18.3701 2.63404 12.0109 7.32234 7.32243C12.011 2.63413 18.37 9.9152e-05 25 9.9152e-05L25.0001 0ZM22.6474 39.1479C22.6474 39.2959 22.7063 39.4382 22.8109 39.5428C22.9155 39.6474 23.0578 39.7063 23.2058 39.7063H26.7951C27.1036 39.7063 27.3535 39.4564 27.3535 39.1479V21.4336C27.3535 21.1255 27.1036 20.8755 26.7951 20.8755H23.2058C23.0578 20.8755 22.9155 20.9344 22.8109 21.0391C22.7063 21.1437 22.6474 21.2855 22.6474 21.4336V39.1479ZM22.6474 15.3344C22.6474 15.4824 22.7063 15.6242 22.8109 15.7292C22.9155 15.8339 23.0578 15.8924 23.2058 15.8924H26.7951C27.1036 15.8924 27.3535 15.6428 27.3535 15.3344V10.8518C27.3535 10.5437 27.1036 10.2938 26.7951 10.2938H23.2058C23.0578 10.2938 22.9155 10.3523 22.8109 10.4573C22.7063 10.5619 22.6474 10.7038 22.6474 10.8518V15.3344Z"/></svg>
+				</span>
+				<h3><?php echo $config['title']; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></h3>
+				<p>
+				<?php echo $config['description']; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
+				</p>
+			</div>
+
+			<style>
+
+				.znpb-el-notice {
+					color: #fff;
+					font-size: 13px;
+					position: relative;
+					background-color: rgba(40, 40, 44, 0.6);
+					border-radius: 4px;
+					padding: 20px 20px 20px 56px;
+					width: 100%;
+					margin: 20px;
+				}
+
+				.znpb-el-notice--error {
+					background-color: #e84655;
+				}
+
+				.znpb-el-notice h3 {
+					font-size: 15px !important;
+					margin: 0 0 5px !important;
+				}
+
+				.znpb-el-notice a {
+					font-weight: 700;
+				}
+
+				.znpb-el-notice .znpb-editor-icon-wrapper {
+					color: rgba(255, 255, 255, 0.4);
+					position: absolute;
+					font-size: 26px;
+					margin-left: -36px;
+				}
+
+				.znpb-el-notice .znpb-editor-icon-wrapper svg {
+					fill: currentColor;
+					width: 1em;
+					height: 1em;
+					display: block;
+				}
+
+			</style>
 		<?php
 	}
 }
